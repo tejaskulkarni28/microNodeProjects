daf2906c9232282f915eed1386757b21bd72b451
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-11-17 03:53:51 +0530

    Just refreshed the EventEmitter

--- eventEmitter/index.js
+++ eventEmitter/index.js
@@ -1,15 +1,23 @@
 // EventEmitter is a built-in module which helps us to create and manage custom events in the application
+//
+// So you have th events waiting in the event-queue.
+// Event loop picks the events smartly for example there are two types of events
+//  1) Blocking Event Operation
+//  2) Non-Blocking Event Operation
+//
+//  For Blocking(Synchronous operation) it is send to the thread pool which have maximum 4 threads. But you can have the cpu threads as an extra threads
+//  For Non-Blocking(Asynchronous operation) it is resloved at the event-loop itself and send as a response back to the client.
+//
+//
+//  // Here comes the EventEmitter which is a built-in module which provides us to create, and manage the events as per the requirement.
+//
+//
+
 const EventEmitter = require('node:events');
-const server = new EventEmitter();
-server.on('eventFirst', (message)=>{
-        console.log(`Here is the message: ${message}`);
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('test', (args)=>{
+        console.log(args)
 })
-server.emit('eventFirst', 'This is an event emitter');
 
-// adding a customer property to the function
-
-function greet(){
-        console.log('Hello!');
-}
-greet.language = "English";
-console.log(greet.language);
+eventEmitter.emit('test', 'SayHello')
